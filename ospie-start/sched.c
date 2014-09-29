50acf6c43ecd48203c3cea32c300a9400931dadd
Martin Remi <rmartin1@if501-219-07.insa-lyon.fr>
2014-09-29 11:54:19 +0200

    Finished q3-6

--- ospie-start/sched.c
+++ ospie-start/sched.c
@@ -1,3 +1,9 @@
 #include "sched.h"
 
 ctx_s* current_ctx;
+
+void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size) {
+	(*ctx).sp = phyAlloc_alloc(stack_size);
+	(*ctx).lr = phyAlloc_alloc(stack_size);
+	(*ctx).f = f;
+}
