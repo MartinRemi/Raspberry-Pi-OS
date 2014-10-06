db60043fc6743eb965c26e733853980824b6c43c
MartinRemi <mrmi11@yahoo.fr>
2014-10-06 08:44:48 +0200

    Finished 4-8

--- ospie-start/sched.c
+++ ospie-start/sched.c
@@ -32,12 +32,12 @@ void __attribute__ ((naked)) switch_to(struct ctx_s* ctx) {
 	__asm("bx lr");
 }
 
-void init_pcb(struct pcb_s* pcb, func_t f, unsigned int stack_size, void* args) {
+void init_pcb(struct pcb_s* pcb, func_t f, unsigned int stack_size, void* args) {	
+	pcb->state = NEW;	
 	pcb->ctx = phyAlloc_alloc(sizeof(struct ctx_s));
 	init_ctx(pcb->ctx, f, stack_size);
 	pcb->size = stack_size;
 	pcb->args = args;
-	pcb->state = NEW;
 }
 
 void create_process(func_t f, void* args, unsigned int stack_size) {
@@ -68,6 +68,12 @@ void start_sched() {
 }
 
 void __attribute__ ((naked)) ctx_switch() {
+	int i = 9;
+
+	// Sauvegarde des registres
+	__asm("push {r0-r12}");	
+
+
 	// Sauvegarde du contexte du current_process
 	if(current_process != NULL) {
 		__asm("mov %0, sp" : "=r"(current_process->ctx->sp));
@@ -82,4 +88,10 @@ void __attribute__ ((naked)) ctx_switch() {
 		__asm("mov sp, %0" : : "r"(current_process->ctx->sp));
 		__asm("mov lr, %0" : : "r"(current_process->ctx->link_register));
 	}
+	
+	// Restauration des registres
+	__asm("pop {r0-r12}");	
+
+	// Sauter à l'adresse de retour
+	__asm("bx lr");
 }
